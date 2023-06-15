5951a4c5dc6e72d6e0a517fd4c19de95bef9cff4
NISTTHA <NISTTHA@LAPTOP-BV5OMR6R>
2023-06-16 00:20:14 +0530

    update3

---
+++ Practice1/src/update3.java
@@ -0,0 +1,9 @@
+
+public class update3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am also fine, How's your studies going on??");
+	}
+
+}
